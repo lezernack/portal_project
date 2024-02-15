6652e345c9ede49ac9d98eff00f39f790f1f18bf
lezernack <luks.ezck@gmail.com>
2024-02-15 13:16:34 -0700

    put in the some javascript code

---
+++ cards.js
@@ -0,0 +1,12 @@
+let cardsList = document.querySelector(".cards");
+let cards = [];
+
+const project = {
+  url: "https://lezernack.github.io/debug-challenge/",
+};
+
+cards.push(project);
+
+cards.map((item) => {
+  cardsList.innerHTML += ``;
+});
--- script.js
+++ script.js
@@ -7,3 +7,18 @@ let currentYear = new Date().getFullYear();
 let year = document.querySelector("#year");
 
 year.textContent = currentYear;
+
+let cardsList = document.querySelector(".cards");
+let cards = [];
+
+const project = {
+  name: "Project1",
+  href: "./project_1/project1.html",
+};
+
+cards.push(project);
+
+cards.map((item) => {
+  cardsList.innerHTML += ``;
+  cards.innerHTML += ``;
+});
